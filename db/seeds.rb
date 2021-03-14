bd9e63754dae2dc175e7b145fa9637a8c4a692ab
parisdolfman <parisdolfman@gmail.com>
2021-03-14 12:49:31 -0400

    Added seed file for testing

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+Painting.create(painting_name: "Guernica", year: "1937", artist_id: 1, user_id: 1)
+Painting.create(painting_name: "Impression Sunrise", year: "1872", artist_id: 2, user_id: 1)
+Painting.create(painting_name: "The Starry Night", year: "1889", artist_id: 3, user_id: 1)
+
+
+puts "data loaded success"
