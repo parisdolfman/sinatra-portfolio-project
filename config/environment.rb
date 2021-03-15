eb5f39cfcf9f51e38fe5e78936dd2536eedd1448
parisdolfman <parisdolfman@gmail.com>
2021-03-15 14:59:52 -0400

    Commented out lines causing db error

--- config/environment.rb
+++ config/environment.rb
@@ -4,18 +4,18 @@ require 'bundler/setup'
 require 'capybara/dsl'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-configure :development do
- set :database, 'sqlite3:db/development.db'
-end
+# configure :development do
+#  set :database, 'sqlite3:db/development.db'
+# end
 
 set :database, {adapter: "sqlite3", database: "development.db"} #fixed issue of rake db:migrate not working (changed from .sqlite3 to .db)
 
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
 #  :database => "db/#{ENV['SINATRA_ENV']}.db" #what was being used last time functioned properly w/o activerecord error 3/14
-  :database => "db/development.db"
-)
+# #  :database => "db/development.db"
+#)
 
   # need a require_all here for controllers in app and 
 
