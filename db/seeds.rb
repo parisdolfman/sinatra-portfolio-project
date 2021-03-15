7edb7c20027c15f6fdaebc8f5fc97ee04aa94b4c
parisdolfman <parisdolfman@gmail.com>
2021-03-15 15:20:43 -0400

    Changed seeds to show different user_id

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 Painting.create(painting_name: "Guernica", year: "1937", artist_id: 1, user_id: 1)
-Painting.create(painting_name: "Impression Sunrise", year: "1872", artist_id: 2, user_id: 1)
-Painting.create(painting_name: "The Starry Night", year: "1889", artist_id: 3, user_id: 1)
+Painting.create(painting_name: "Impression Sunrise", year: "1872", artist_id: 2, user_id: 2)
+Painting.create(painting_name: "The Starry Night", year: "1889", artist_id: 3, user_id: 3)
 
 
 puts "data loaded success"
