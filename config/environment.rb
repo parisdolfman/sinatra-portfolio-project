d68c897e4384d2b849f7dfd461ea5398dbd63f76
parisdolfman <parisdolfman@gmail.com>
2021-03-16 09:04:01 -0400

    Removed commented out code not in use for cleaner readability

--- config/environment.rb
+++ config/environment.rb
@@ -4,19 +4,9 @@ require 'bundler/setup'
 require 'capybara/dsl'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-# configure :development do
-#  set :database, 'sqlite3:db/development.db'
-# end
 
-set :database, {adapter: "sqlite3", database: "development.db"} #fixed issue of rake db:migrate not working (changed from .sqlite3 to .db)
+set :database, {adapter: "sqlite3", database: "development.db"} 
 
 
-# ActiveRecord::Base.establish_connection(
-#   :adapter => "sqlite3",
-#  :database => "db/#{ENV['SINATRA_ENV']}.db" #what was being used last time functioned properly w/o activerecord error 3/14
-# #  :database => "db/development.db"
-#)
-
-  # need a require_all here for controllers in app and 
 
   require_all 'app'
